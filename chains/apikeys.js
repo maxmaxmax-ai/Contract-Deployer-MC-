4869fbb52deb75a3156d4c1dc3512702af9b0a7b
MaXmAx <dubalvaldez2018@gmail.com>
2025-09-25 05:44:59 -0400

    Update apikeys.js

--- chains/apikeys.js
+++ chains/apikeys.js
@@ -1,12 +1,12 @@
 module.exports = {
-    basescan: 'H6QXNGUSYVJJUT9FGNJ159NZEY17W95TH3', //Base
-    etherscan: 'I6KUVDGNFDSQT9ZZJHST11BEUI4PK1BDAR', // Ethereum
-    optimistic_etherscan: '', // Optimisc
-    arbiscan: 'I59G6ESYJ3J1HEWMJYABRXFUBUMJ77SWPI', // Arbitrum
-    bscscan: '4ICP1GEZERNJ7NP489CFFG5H3XYF5ZHFKC', // Binance Smart Chain
+    basescan: '', //Base
+    etherscan: '', // Ethereum
+    optimisc: '', // Optimisc
+    arbiscan: '', // Arbitrum
+    bscscan: '', // Binance Smart Chain
     snowtrace: '', // Avalanche
-    polygonscan: 'WZDMG26B1Q3XD29R6CREF8GCCDGHYQ9FS5', // Polygon
-    ftmscan: 'd6eae8bf-be78-4213-ba77-020f61539e85', // Fantom
+    polygonscan: '', // Polygon
+    ftmscan: '', // Fantom
     hecoinfo: '', // Hecochain
     moonscan: '', // Moonbeam
     bttcscan: '', // Bittorrent
